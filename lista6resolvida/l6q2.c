b6fe1374454c92c7ec90902e125dcb93d5e0a8ec
Edvan3557 <53985039+Edvan3557@users.noreply.github.com>
2019-10-10 20:42:10 -0300

    Update l6q2.c

--- lista6resolvida/l6q2.c
+++ lista6resolvida/l6q2.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+void main(){
+    char texto[50];
+    int x;
 
+    printf("digite uma frase:");
+    gets(texto);
+    x=0;
+    while(texto[x] != '\0'){
+    x++;
+    }
+    printf("O numero de caracteres e %d",x);
+}
