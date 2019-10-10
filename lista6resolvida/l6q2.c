97a726d713bf3ba44d0016e6058917f84ac7c61e
Edvan3557 <53985039+Edvan3557@users.noreply.github.com>
2019-10-10 20:56:21 -0300

    Update l6q2.c

--- lista6resolvida/l6q2.c
+++ lista6resolvida/l6q2.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main(){
-    char texto[50];
-    int x;
+    char texto[50];//variavel do tipo nome//
+    int x;//variavel inteira numeros//
 
-    printf("digite uma frase:");
-    gets(texto);
+    printf("digite uma frase:"); //printa o que se pede
+    gets(texto);//entrada de daddos//
     x=0;
-    while(texto[x] != '\0'){
-    x++;
+    while(texto[x] != '\0'){//comando de repeticões//
+    x++;//atribuicão//
     }
     printf("O numero de caracteres e %d",x);
 }
